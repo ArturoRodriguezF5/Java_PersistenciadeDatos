d891d64458a9c0e9d52cc1eadc7c37a357ea182f
Arturo Rodríguez Flores <114115632+ArturoRodriguezF5@users.noreply.github.com>
2023-04-22 12:41:37 -0600

    Actualizacion para repo que contiene ejemplos y ejercicios elaborados con Java persistencia de datos.

---
+++ jdbc/src/main/java/com/arthycode/jdbc/EjemploJDBC.java
@@ -0,0 +1,35 @@
+package com.arthycode.jdbc;
+
+import java.sql.*;
+
+public class EjemploJDBC {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/mensajes_app?serverTimezone=UTC";
+        String username = "root";
+        String password = "";
+
+
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            Statement statement = connection.createStatement();
+            System.out.println("Conexión establecida con éxito.");
+            try {
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM mensajes");
+                System.out.println("Si se encontró");
+                while (resultSet.next()) {
+                    System.out.print("Id: " + resultSet.getString("id_mensaje"));
+                    System.out.print(" | Mensaje: " + resultSet.getString("mensaje"));
+                    System.out.print(" | Autor_Mensaje: " + resultSet.getString("autor_mensaje"));
+                    System.out.print(" | Fecha del mensaje: " + resultSet.getString("fecha_mensaje"));
+                }
+                connection.close();
+                resultSet.close();
+                statement.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        } catch (SQLException e) {
+            System.out.println("Error conexión fallida." + e.getMessage());
+        }
+    }
+}
